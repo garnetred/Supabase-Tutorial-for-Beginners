f07fa471061d11e7d575b1c406c3011f30284dbd
December G <dgarnetsmith@gmail.com>
2022-10-07 23:31:20 -0600

    Add supabase to project

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,45 @@
+import supabase from "../config/supabaseClient";
+import { useEffect, useState } from "react";
+
+// components
+import SmoothieCard from "../components/SmoothieCard";
+
 const Home = () => {
+  const [error, setError] = useState(null);
+  const [smoothies, setSmoothies] = useState(null);
+
+  useEffect(() => {
+    const fetchSmoothies = async () => {
+      const { data, error } = await supabase.from("smoothies").select();
+      if (error) {
+        setError("Could not fetch smoothies");
+        setSmoothies(null);
+        console.log(error);
+      }
+
+      if (data) {
+        setSmoothies(data);
+        setError(null);
+      }
+    };
+    fetchSmoothies();
+  }, []);
+
   return (
     <div className="page home">
-      <h2>Home</h2>
+      {error && <div>{error}</div>}
+      {smoothies && (
+        <div className="smoothies">
+          {/* order by button */}
+          <div className="smoothie-grid">
+            {smoothies.map((smoothie) => (
+              <SmoothieCard smoothie={smoothie} key={smoothie.id} />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
